561b0224977ef786f4d204ccc25a715b86220e9c
ab-amar <68697482+ab-amar@users.noreply.github.com>
2020-08-03 11:58:14 +0530

    Update script.js

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -96,7 +96,7 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
+    function (homeHtmlUrl) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
