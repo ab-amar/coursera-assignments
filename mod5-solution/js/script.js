e4a9ee05748b4b007bdbdf9143e3ed0c993c54fa
ab-amar <68697482+ab-amar@users.noreply.github.com>
2020-08-03 12:06:30 +0530

    Update script.js

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -96,12 +96,12 @@ function buildAndShowHomeHTML (categories) {
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtmlUrl) {
+    function (homeHtml) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-       var chosenCategoryShortName = ""+chooseRandomCategory(categories).short_name;
+       var chosenCategoryShortName = "'"+chooseRandomCategory(categories).short_name+"'";
        
 
 
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-       var  homeHtmlToInsertIntoMainPage= insertProperty(homeHtmlUrl,"randomCategoryShortName",chosenCategoryShortName);
+       var  homeHtmlToInsertIntoMainPage= insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
        
 
 
